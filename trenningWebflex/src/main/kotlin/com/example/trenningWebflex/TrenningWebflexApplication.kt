c54bbcf97cbe976d33f4a348519d0efebe07c60b
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-07-09 22:00:45 +0700

    Initial commit

---
+++ trenningWebflex/src/main/kotlin/com/example/trenningWebflex/TrenningWebflexApplication.kt
@@ -0,0 +1,31 @@
+package com.example.trenningWebflex
+
+import com.example.trenningWebflex.model.Aircraft
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisOperations
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@SpringBootApplication
+
+class TrenningWebflexApplication{
+	@Bean
+	fun redisOperations(factory: RedisConnectionFactory?): RedisOperations<String, Aircraft>? {
+		val serializer = Jackson2JsonRedisSerializer(Aircraft::class.java)
+
+		val template = RedisTemplate<String, Aircraft>()
+		template.setConnectionFactory(factory!!)
+		template.setDefaultSerializer(serializer)
+		template.keySerializer = StringRedisSerializer()
+
+		return template
+	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<TrenningWebflexApplication>(*args)
+}
